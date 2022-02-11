d68d41acfb37eebbbc5bab931960b0c5f066cfb0
EvanKoe <evan.koehler@epitech.eu>
2022-02-11 19:42:53 +0100

    making bubbles

--- Screens/Conv.tsx
+++ Screens/Conv.tsx
@@ -1,10 +1,15 @@
+import { setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
 import React from 'react';
 import { View, Text, FlatList, ListRenderItem, ListRenderItemInfo, StyleSheet } from 'react-native';
 import { ConvProps, Message } from '../Globals/types';
 
 const Bubble = (e: ListRenderItemInfo<Message>) => {
+  const sender = e.item.sender;
+
   return (
-    <Text>{e.item.body }</Text>
+    <View style={[ styles.bubble, sender === 'you' ? styles.you : sender === 'monke' ? styles.monke : styles.success ]}>
+      <Text>{e.item.body}</Text>
+    </View>
   )
 };
 
@@ -21,7 +26,26 @@ const Conv = ({ messages = [] }: ConvProps) => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#121212'
+    backgroundColor: '#121212',
+    marginHorizontal: 5
+  },
+  bubble: {
+    backgroundColor: '#414141',
+    padding: 10,
+    marginTop: 5,
+    borderRadius: 15,
+  },
+  you: {
+    backgroundColor: '#4444cc',
+    borderBottomRightRadius: 5
+  },
+  monke: {
+    backgroundColor: '#313131',
+    borderBottomLeftRadius: 5
+  },
+  success: {
+    backgroundColor: '#bb2222',
+    borderRadius: 5
   }
 });
 
