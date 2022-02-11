fa679a37dd9873bb019b731086c5f7121bd730da
cedric ulrich <cedric.ulrich@epitech.eu>
2022-02-11 22:12:23 +0100

    ScrolView

--- Screens/Sender.tsx
+++ Screens/Sender.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet, ScrollView, Button,TouchableOpacity } from 'react-native'
+import colors from '../Globals/colors';
 
 const Sender = () => {
-  return (
-    <Text style={styles.container}> PUTE </Text>
-  );
+    return (
+      <ScrollView horizontal={true} style={ styles.container}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={{fontSize:20}}>Solution 1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={{fontSize:20}}>Solution 2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={{fontSize:20}}>Solution 3</Text>
+          </TouchableOpacity>
+        </ScrollView>
+    );
 };
 
 const styles = StyleSheet.create({
   container: {
-    color: '#fff'
+    alignSelf: 'center',
+    color: '#fff',
+    maxHeight: '15%'
+  },
+  button: {
+    alignSelf: 'center',
+    marginHorizontal: 30,
+    backgroundColor: colors.bg,
+    borderColor: colors.bg,
+    borderRadius: 15,
+    borderWidth: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 20
   }
 });
 
