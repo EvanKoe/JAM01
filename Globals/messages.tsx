1b15f0b0cabd240ad68a1314d6399fbbbd572892
Skywear <alexandre.barbier@epitech.eu>
2022-02-12 16:39:50 +0100

    [ADD] 1st messages

--- Globals/messages.tsx
+++ Globals/messages.tsx
@@ -8,28 +8,64 @@ const messages: Message[] = [
     body: "Hi, human"
   },
   {
-    stage: 0,
-    answerId: -1,
-    sender: "success",
-    body: "You're successful motherfucker"
-  },
-  {
     stage: 1,
     answerId: 0,
     sender: "you",
-    body: "oh fuck, I'm successful"
+    body: "Hi monke !"
   },
   {
     stage: 1,
     answerId: 1,
     sender: "you",
-    body: "je suce"
+    body: "Are you a real monke ?"
   },
   {
     stage: 1,
     answerId: 2,
     sender: "you",
-    body: "archi est bô"
+    body: "Archi, is that you ?!"
+  },
+  {
+    stage: 1,
+    answerId: -1,
+    sender: "success",
+    body: "First sms sent to the monke !"
+  },
+  {
+    stage: 1,
+    answerId: 1,
+    sender: "monke",
+    body: "Yes I am !"
+  },
+  {
+    stage: 1,
+    answerId: 2,
+    sender: "monke",
+    body: "No fucking faggot, I am just a monke"
+  },
+  {
+    stage: 1,
+    answerId: 0,
+    sender: "monke",
+    body: "How are you, human ?"
+  },
+  {
+    stage: 1,
+    answerId: 2,
+    sender: "success",
+    body: "You confused Archi with a monkey."
+  },
+  {
+    stage: 2,
+    answerId: 0,
+    sender: "you",
+    body: "coucou"
+  },
+  {
+    stage: 2,
+    answerId: 1,
+    sender: "you",
+    body: "bite"
   }
 ];
 
