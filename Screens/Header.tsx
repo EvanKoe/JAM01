9bb3492ebfc93c75a4f0335e2fd7fa64740232d7
Ario <pierrick.cheviron@epitech.eu>
2022-02-14 04:40:01 +0100

    correction of achiviement's bug

--- Screens/Header.tsx
+++ Screens/Header.tsx
@@ -38,7 +38,7 @@ const Header = ({
               return e;
             });
 
-            setSuccessGlobal(successGlobal + success);
+            setSuccessGlobal(success);
             setFullMessages(a);
             setMessages(a);
             setStage(0);
