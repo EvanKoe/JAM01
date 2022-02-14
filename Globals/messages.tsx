e8d6764c5a270b562091cfd65da85f204ace5434
Ario <pierrick.cheviron@epitech.eu>
2022-02-14 02:41:06 +0100

    test messages

--- Globals/messages.tsx
+++ Globals/messages.tsx
@@ -1,6 +1,9 @@
 import { Message } from "./types";
 
 const messages: Message[] = [
+  /////
+  /////
+  /////
   {
     stage: 0,
     answerId: -1,
@@ -29,13 +32,9 @@ const messages: Message[] = [
     sender: "you",
     body: "Archi, is that you ?!"
   },
-  {
-    stage: 2,
-    answerId: -1,
-    isDisplayed: true,
-    sender: "success",
-    body: "First sms sent to the monke !"
-  },
+  /////
+  /////
+  /////
   {
     stage: 2,
     answerId: 1,
@@ -59,24 +58,303 @@ const messages: Message[] = [
   },
   {
     stage: 2,
+    answerId: -1,
+    isDisplayed: true,
+    sender: "success",
+    body: "First sms sent to the monke !"
+  },
+  {
+    stage: 2,
     answerId: 2,
     isDisplayed: false,
     sender: "success",
-    body: "You confused Archi with a monkey. This will be reported."
+    body: "You confused Archi with a monkey. This \n will be reported."
   },
+  /////
+  /////
+  /////
   {
     stage: 3,
     answerId: 0,
     isDisplayed: false,
     sender: "you",
-    body: "coucou"
+    body: "Monke do not usually talk !"
   },
   {
     stage: 3,
     answerId: 1,
     isDisplayed: false,
     sender: "you",
-    body: "bwa bwaaaaaaaaaa"
+    body: "Answer 2"
+  },
+  {
+    stage: 3,
+    answerId: 2,
+    isDisplayed: false,
+    sender: "you",
+    body: "answer 3"
+  },
+  /////
+  /////
+  /////
+  {
+    stage: 4,
+    answerId: 0,
+    isDisplayed: false,
+    sender: "monke",
+    body: "answer 1"
+  },
+  {
+    stage: 4,
+    answerId: 0,
+    isDisplayed: false,
+    sender: "success",
+    body: "Prout1"
+  },
+  {
+    stage: 4,
+    answerId: 1,
+    isDisplayed: false,
+    sender: "monke",
+    body: "answer 2"
+  },
+  {
+    stage: 4,
+    answerId: 1,
+    isDisplayed: false,
+    sender: "success",
+    body: "prout2"
+  },
+  {
+    stage: 4,
+    answerId: 2,
+    isDisplayed: false,
+    sender: "monke",
+    body: "answer 3"
+  },
+  {
+    stage: 4,
+    answerId: 2,
+    isDisplayed: false,
+    sender: "success",
+    body: "prout3"
+  },
+
+  /////
+  /////
+  /////
+  {
+    stage: 5,
+    answerId: 0,
+    isDisplayed: false,
+    sender: "you",
+    body: "answer 11"
+  },
+  {
+    stage: 5,
+    answerId: 1,
+    isDisplayed: false,
+    sender: "you",
+    body: "answer 22"
+  },
+  {
+    stage: 5,
+    answerId: 2,
+    isDisplayed: false,
+    sender: "you",
+    body: "answer 33"
+  },
+  //////
+  //////
+  //////
+
+  {
+    stage: 6,
+    answerId: 0,
+    isDisplayed: false,
+    sender: "monke",
+    body: "answer 11"
+  },
+  {
+    stage: 6,
+    answerId: 0,
+    isDisplayed: false,
+    sender: "success",
+    body: "prout11"
+  },
+  {
+    stage: 6,
+    answerId: 1,
+    isDisplayed: false,
+    sender: "monke",
+    body: "answer 22"
+  },
+  {
+    stage: 6,
+    answerId: 1,
+    isDisplayed: false,
+    sender: "success",
+    body: "prout22"
+  },
+  {
+    stage: 6,
+    answerId: 2,
+    isDisplayed: false,
+    sender: "monke",
+    body: "answer 33"
+  },
+  {
+    stage: 6,
+    answerId: 2,
+    isDisplayed: false,
+    sender: "success",
+    body: "prout33"
+  },
+
+  //////
+  //////
+  //////
+
+  {
+    stage: 7,
+    answerId: 0,
+    isDisplayed: false,
+    sender: "you",
+    body: "answer 111"
+  },
+  {
+    stage: 7,
+    answerId: 1,
+    isDisplayed: false,
+    sender: "you",
+    body: "answer 222"
+  },
+  {
+    stage: 7,
+    answerId: 2,
+    isDisplayed: false,
+    sender: "you",
+    body: "answer 333"
+  },
+
+  /////
+  /////
+  /////
+
+  {
+    stage: 8,
+    answerId: 0,
+    isDisplayed: false,
+    sender: "monke",
+    body: "answer 111"
+  },
+  {
+    stage: 8,
+    answerId: 0,
+    isDisplayed: false,
+    sender: "success",
+    body: "prout111"
+  },
+  {
+    stage: 8,
+    answerId: 1,
+    isDisplayed: false,
+    sender: "monke",
+    body: "answer 222"
+  },
+  {
+    stage: 8,
+    answerId: 1,
+    isDisplayed: false,
+    sender: "success",
+    body: "prout222"
+  },
+  {
+    stage: 8,
+    answerId: 2,
+    isDisplayed: false,
+    sender: "monke",
+    body: "answer 333"
+  },
+  {
+    stage: 8,
+    answerId: 2,
+    isDisplayed: false,
+    sender: "success",
+    body: "prout333"
+  },
+
+  //////
+  //////
+  //////
+
+  {
+    stage: 9,
+    answerId: 0,
+    isDisplayed: false,
+    sender: "you",
+    body: "answer 1111"
+  },
+  {
+    stage: 9,
+    answerId: 1,
+    isDisplayed: false,
+    sender: "you",
+    body: "answer 2222"
+  },
+  {
+    stage: 9,
+    answerId: 2,
+    isDisplayed: false,
+    sender: "you",
+    body: "answer 3333"
+  },
+
+  ////
+  ////
+  /////
+
+  {
+    stage: 10,
+    answerId: 0,
+    isDisplayed: false,
+    sender: "monke",
+    body: "answer 1111"
+  },
+  {
+    stage: 10,
+    answerId: 0,
+    isDisplayed: false,
+    sender: "success",
+    body: "prout1111"
+  },
+  {
+    stage: 10,
+    answerId: 1,
+    isDisplayed: false,
+    sender: "monke",
+    body: "answer 2222"
+  },
+  {
+    stage: 10,
+    answerId: 1,
+    isDisplayed: false,
+    sender: "success",
+    body: "prout2222"
+  },
+  {
+    stage: 10,
+    answerId: 2,
+    isDisplayed: false,
+    sender: "monke",
+    body: "answer 3333"
+  },
+  {
+    stage: 10,
+    answerId: 2,
+    isDisplayed: false,
+    sender: "success",
+    body: "prout3333"
   }
 ];
 
